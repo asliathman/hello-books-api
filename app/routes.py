bde7f6769aed0242707fa1a7a1cb0df73911389f
Asli Athman <asliathman@gmail.com>
2021-10-22 13:26:59 -0400

    added books model,+ migration, updated __init__.py

--- app/routes.py
+++ app/routes.py
@@ -1,42 +1,82 @@
-from flask import Blueprint, jsonify
-
-hello_world_bp = Blueprint("hello_world", __name__)
-
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def say_hello_world():
-    my_beautiful_response_body = "Hello, World!"
-    return my_beautiful_response_body
-
-@hello_world_bp.route("/hello/JSON", methods=["GET"])
-def say_hello_json():
-    return {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-
-
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
-
-books = [
-    Book(1, "Simulacra and Simulation", "Philosophical examination of the relationship between reality, symbols and society."),
-    Book(2, "Beautiful World, Where Are You", "Novel about two best friends and the two men with whom they test tentative new romantic relationships."),
-    Book(3, "Design Patterns", "Software engineering book describe software design patterns.")
-]
+#BUILDING API PT 2 
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["GET"])
+@books_bp.route("", methods=["GET", "POST"])
 def handle_books():
-    books_response = []
-    for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
-    return jsonify(books_response)
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        return make_response(f"Book {new_book.title} successfully created", 201)
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = Book.query.get(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
+
+#ORIGINAL HELLO-BOOKS
+# from flask import Blueprint, jsonify
+
+# hello_world_bp = Blueprint("hello_world", __name__)
+
+# @hello_world_bp.route("/hello-world", methods=["GET"])
+# def say_hello_world():
+#     my_beautiful_response_body = "Hello, World!"
+#     return my_beautiful_response_body
+
+# @hello_world_bp.route("/hello/JSON", methods=["GET"])
+# def say_hello_json():
+#     return {
+#         "name": "Ada Lovelace",
+#         "message": "Hello!",
+#         "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+#     }
+
+
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
+
+# books = [
+#     Book(1, "Simulacra and Simulation", "Philosophical examination of the relationship between reality, symbols and society."),
+#     Book(2, "Beautiful World, Where Are You", "Novel about two best friends and the two men with whom they test tentative new romantic relationships."),
+#     Book(3, "Design Patterns", "Software engineering book describe software design patterns.")
+# ]
+
+# books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+# @books_bp.route("", methods=["GET"])
+# def handle_books():
+#     books_response = []
+#     for book in books:
+#         books_response.append({
+#             "id": book.id,
+#             "title": book.title,
+#             "description": book.description
+#         })
+#     return jsonify(books_response)
