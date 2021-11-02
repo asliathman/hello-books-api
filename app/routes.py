fb7812555a22873c63efbdb596cdf1c4e066ca92
Asli Athman <asliathman@gmail.com>
2021-11-02 11:14:36 -0400

    cleaned up

--- app/routes.py
+++ app/routes.py
@@ -62,46 +62,4 @@ def handle_book(book_id):
         return make_response(f"Book #{book.id} successfully deleted")
 
     
-#ORIGINAL HELLO-BOOKS
-# from flask import Blueprint, jsonify
 
-# hello_world_bp = Blueprint("hello_world", __name__)
-
-# @hello_world_bp.route("/hello-world", methods=["GET"])
-# def say_hello_world():
-#     my_beautiful_response_body = "Hello, World!"
-#     return my_beautiful_response_body
-
-# @hello_world_bp.route("/hello/JSON", methods=["GET"])
-# def say_hello_json():
-#     return {
-#         "name": "Ada Lovelace",
-#         "message": "Hello!",
-#         "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-#     }
-
-
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
-
-# books = [
-#     Book(1, "Simulacra and Simulation", "Philosophical examination of the relationship between reality, symbols and society."),
-#     Book(2, "Beautiful World, Where Are You", "Novel about two best friends and the two men with whom they test tentative new romantic relationships."),
-#     Book(3, "Design Patterns", "Software engineering book describe software design patterns.")
-# ]
-
-# books_bp = Blueprint("books", __name__, url_prefix="/books")
-
-# @books_bp.route("", methods=["GET"])
-# def handle_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append({
-#             "id": book.id,
-#             "title": book.title,
-#             "description": book.description
-#         })
-#     return jsonify(books_response)
